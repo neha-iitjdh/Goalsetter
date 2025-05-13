368306d6e9651e8421ab32c5f05380b81855eee6
Neha Sharma <neha.iitjdh@gmail.com>
2025-05-13 20:29:56 +0530

    Initial GET rounting done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+res.send('Get goals');
+})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
