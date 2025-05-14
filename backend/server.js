219071b5fe5db6b6803af7e4403a7abe191ee57a
Neha Sharma <neha.iitjdh@gmail.com>
2025-05-14 15:27:04 +0530

    Express Async Handler Activated

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
 // Middleware to parse JSON requests
 
+app.use(errorHandler);
+// Error handling middleware
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
