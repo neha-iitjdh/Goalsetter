ea63c6ae034650919f7cf3c01ec6b0b894675315
Neha Sharma <neha.iitjdh@gmail.com>
2025-05-15 00:14:26 +0530

    Database Issue fixed from Test to GoalSetter

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -46,7 +46,13 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+  await goal.deleteOne();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
