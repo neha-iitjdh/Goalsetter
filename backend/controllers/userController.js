b34b94bdd5499317820359862773ece1aab1d692
Neha Sharma <neha.iitjdh@gmail.com>
2025-05-17 16:06:43 +0530

    bcrypt & jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,23 @@
+// @desc    Register a new user
+// @route: /api/users
+// @access: Public
+
 const registerUser = (req, res) => {
   res.json({ message: "Register User" });
 };
+
+// @desc    Login a user
+// @route: /api/users/login
+// @access: Public
+
 const loginUser = (req, res) => {
   res.json({ message: "Login User" });
 };
+
+// @desc    Get user data
+// @route: /api/users/me
+// @access: Public
+
 const getMe = (req, res) => {
   res.json({ message: "User data" });
 };
