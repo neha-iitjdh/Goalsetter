ff3e2e30105bd87ca81cee0c9591d5f78a73e2fa
Neha Sharma <neha.iitjdh@gmail.com>
2025-06-14 18:22:42 +0530

    Frontend Issues Resolved

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,15 +1,15 @@
-import { render } from "@testing-library/react";
-import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
-  // eslint-disable-next-line testing-library/prefer-screen-queries
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(getByText(/learn/i)).toBeInTheDocument();
 });
