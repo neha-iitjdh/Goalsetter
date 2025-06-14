c010bb59a67a63f9ebddcf6741974c0a5e3e8b3a
Neha Sharma <neha.iitjdh@gmail.com>
2025-06-14 18:15:20 +0530

    Backend Issues Resolved

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,22 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const goalSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: "User",
+      ref: 'User',
     },
-    // The user field is a reference to the User model, which allows us to associate each goal with a specific user.
     text: {
       type: String,
-      required: [true, "Please add a text value"],
+      required: [true, 'Please add a text value'],
     },
   },
   {
     timestamps: true,
   }
-);
-const Goal = mongoose.model("Goal", goalSchema);
-module.exports = Goal;
-// This code defines a Mongoose schema and model for a "Goal" entity. The schema specifies that each goal has a required text field, and timestamps will be automatically added to each document. The model is then exported for use in other parts of the application.
+)
+
+module.exports = mongoose.model('Goal', goalSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,34 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const connectDB = require("./config/db");
-const colors = require("colors");
-const { errorHandler } = require("./middleware/errorMiddleware");
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
-connectDB(); // Connect to the database
-// Initialize the Express application
+connectDB();
 
 const app = express();
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/goals", require("./routes/goalRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
-// Middleware to parse JSON requests
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.use(errorHandler);
-// Error handling middleware
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
