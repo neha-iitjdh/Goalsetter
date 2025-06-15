5b84388e485008885ea2613c94500a2183c3588d
Neha Sharma <neha.iitjdh@gmail.com>
2025-06-15 15:19:27 +0530

    Enabled CORS

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const colors = require('colors');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
@@ -9,6 +10,10 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(cors({
+  origin: 'https://goalsetter-delta.vercel.app/',
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
